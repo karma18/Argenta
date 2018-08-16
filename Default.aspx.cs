a6bf1c94a767be0c71c56eb2b159f24004d87830
Karla Marin <karla.marin@autocom.mx>
2018-08-16 17:20:05 -0500

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArgentaWebServices
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonClientws_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ClientWs.aspx");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArgentaWebServices.Global" Language="C#" %>
